18e82fa0d8926b4e7dc969462b1cf13a5dd3e116
Onur Gokkocabas <onurgokkocabas@gmail.com>
2020-10-20 12:11:28 -0400

    Update CreateMultibranchPipelines.groovy

--- pipelines_lib/dsl/CreateMultibranchPipelines.groovy
+++ pipelines_lib/dsl/CreateMultibranchPipelines.groovy
@@ -2,10 +2,10 @@ import groovy.json.JsonSlurper;
 
 // Create a map of environment and namespace associated
 def envNsMap = [
-  "useast-duluth-nonprod-green-app": ["dev", "test"],
-  "useast-duluth-nonprod-blue-app": ["dev", "test"],
-  "useast-duluth-prod-green-app": ["preprod", "prod"],
-  "useast-duluth-prod-blue-app": ["preprod", "prod"],
+  "foo1": ["dev", "test"],
+  "bar1": ["dev", "test"],
+  "foo2": ["preprod", "prod"],
+  "bar2": ["preprod", "prod"],
   "blue-sandbox": ["dev", "test"],
   "green-sandbox": ["dev", "test"]
 ]
@@ -14,25 +14,25 @@ def getPriURL() {
   try{
     List<String> repoList = new ArrayList<String>()
     
-    URL apiUrl1 = "https://git01.pfsfhq.com/api/v3/orgs/pri/repos?access_token=6385b82adaaf13b0d1d96aa7b5a2a904ce9f7551".toURL()
+    URL apiUrl1 = "foo1".toURL()
     List json1 = new JsonSlurper().parse(apiUrl1.newReader())
     for (repo in json1) { 
       repoList.add('pri/'+repo.name) 
     }
 
-    URL apiUrl2 = "https://git01.pfsfhq.com/api/v3/orgs/ClientPortal/repos?access_token=6385b82adaaf13b0d1d96aa7b5a2a904ce9f7551".toURL()
+    URL apiUrl2 = "bar1".toURL()
     List json2 = new JsonSlurper().parse(apiUrl2.newReader())
     for (repo in json2) { 
       repoList.add('ClientPortal/'+repo.name) 
     }
     
-    URL apiUrl3 = "https://git01.pfsfhq.com/api/v3/orgs/NextGen/repos?access_token=6385b82adaaf13b0d1d96aa7b5a2a904ce9f7551".toURL()
+    URL apiUrl3 = "foo2".toURL()
     List json3 = new JsonSlurper().parse(apiUrl3.newReader())
     for (repo in json3) { 
       repoList.add('NextGen/'+repo.name) 
     }
     
-    URL apiUrl4 = "https://git01.pfsfhq.com/api/v3/orgs/Reins/repos?access_token=6385b82adaaf13b0d1d96aa7b5a2a904ce9f7551".toURL()
+    URL apiUrl4 = "bar2".toURL()
     List json4 = new JsonSlurper().parse(apiUrl4.newReader())
     for (repo in json4) { 
       repoList.add('Reins/'+repo.name) 
@@ -53,10 +53,10 @@ for (env in envNsMap) {
   
   for (ns in envNsMap[env.key]) {
     folder("Application_Deployments/${env.key}/${ns}")
-    folder("Application_Deployments/${env.key}/${ns}/pri")
-    folder("Application_Deployments/${env.key}/${ns}/ClientPortal")
-    folder("Application_Deployments/${env.key}/${ns}/NextGen")
-    folder("Application_Deployments/${env.key}/${ns}/Reins")
+    folder("Application_Deployments/${env.key}/${ns}/a")
+    folder("Application_Deployments/${env.key}/${ns}/b")
+    folder("Application_Deployments/${env.key}/${ns}/c")
+    folder("Application_Deployments/${env.key}/${ns}/d")
     
     for (repo in getPriURL()) {
       multibranchPipelineJob("Application_Deployments/${env.key}/${ns}/${repo}") {
@@ -65,8 +65,8 @@ for (env in envNsMap) {
             source {
               git {
                 id(repo)
-                remote('https://git01.pfsfhq.com/'+repo+'.git')
-                credentialsId('a813d979-f746-4065-bd49-2a103dd6217d')
+                remote('https://github.com/'+repo+'.git')
+                credentialsId('abc-foo-bar-id')
                 traits {
                   headWildcardFilter {
                     includes('*')
